89bc78d3c0b19bb4b0ccc54dc6706000c63f9ff2
KaungThuLin1996 <kaungthu31@gmail.com>
2020-04-09 14:15:52 +0630

    Initial commit

---
+++ src/main/java/com/shop/ecommerce/affablebean/AffablebeanApplication.java
@@ -0,0 +1,13 @@
+package com.shop.ecommerce.affablebean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffablebeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AffablebeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/ecommerce/affablebean/AffablebeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.ecommerce.affablebean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffablebeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
